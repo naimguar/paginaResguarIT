1250c5b731d1b2f2d2d0e5d5fde3008afaee7c63
naimguar <naimguar@gmail.com>
2024-11-08 13:12:26 -0300

    Componente de Redes Sociales terminado, falta error linea titilando

---
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './socialmedia.css';
+import instagramLogo from '../../assets/logoinstagram.png';
+import facebookLogo from '../../assets/logofacebook.png';
+import linkedinLogo from '../../assets/logolinkedin.png';
+import whatsappLogo from '../../assets/logowhatsapp.png';
+
+function SocialMedia() {
+    return (
+        <div className="social-links">
+            <a href="https://www.instagram.com/resguarit/" target="_blank" className="social-icon"><img src={instagramLogo} alt="Instagram Logo"/></a>
+            <a href="https://web.facebook.com/resguarit" target="_blank" className="social-icon"><img src={facebookLogo} alt="Facebook Logo"/></a>
+            <a href="https://www.linkedin.com/company/resguarit" target="_blank" className="social-icon"><img src={linkedinLogo} alt="Linkedin Logo"/></a>
+            <a href="https://api.whatsapp.com/send?phone=5492216914649" target="_blank" className="social-icon"><img src={whatsappLogo} alt="Whatsapp Logo"/></a>
+        </div>
+    );
+}
+
+export default SocialMedia;
