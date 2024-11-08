d70797e7ddc70d683cecdad67fa505761a02a638
Mariano <marianosalas24@gmail.com>
2024-11-08 13:38:26 -0300

    Componente Card implementado

--- src/components/Aplicacion.jsx
+++ src/components/Aplicacion.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import fondoPantalla from '../assets/fondoPantalla.mp4';
 import '../styles/app.css';
 import Navbar from './Navbar/Navbar';
+import Card from './Card/Card';
 
 function Aplicacion() {
     return (
@@ -11,6 +12,11 @@ function Aplicacion() {
                     <source src={fondoPantalla} type="video/mp4" />
                 </video>
                 <Navbar />
+                <div className='contenedor-cartas'>
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
             </div>
         </>
     );
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Card.css'
+
+function Card(){
+return(
+<div className="container-card">
+    <div className="nombre-card">PROYECTO 1</div>
+    <div className="contenido-card">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    </div>
+</div>
+)
+}
+export default Card;
